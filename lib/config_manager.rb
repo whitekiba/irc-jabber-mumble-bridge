6270e61ba8a1cbd5e07a85a178c060c9c5cdaff5
Sebastian Wessalowski <sebastian@wessalowski.org>
2016-05-30 22:31:35 +0200

    Config für Datenbank und Redis.
    Die configManager Klasse nutzt nun die config und sollte so auch
    funktionieren. Ist aber erst mal ungetestet

--- lib/config_manager.rb
+++ lib/config_manager.rb
@@ -1,22 +1,29 @@
 require 'mysql2'
+require 'yaml'
 
 class ConfigManager
   def initialize
-    @db = Mysql2::Client.new(:host => "127.0.0.1", :username => "root")
-    @db.select_db('bridge')
+    @config = YAML.load_file(File.dirname(__FILE__) + '/../config.yml')[:database]
+    @db = Mysql2::Client.new(:host => @config[:host], :username => @config[:user], :password => @config[:password])
+    @db.select_db(@config[:database])
   end
   def loadService(service)
-
+    services = Array.new
+    res = @db.query("SELECT * FROM services WHERE service LIKE '#{service}'");
+    res.each do |entry|
+      services[entry["service"]] = entry["user_ID"]
+    end
+    services
   end
-  def addServiceToUser(user_id, service, ident_value, server = nil)
-    @db.query("")
+  def addServiceToUser(user_id, service, ident_value, server = nil, server_username = nil)
+    @db.query("INSERT INTO `services` (`ID`, `user_ID`, `ident_value`, `channel`) VALUES (NULL, '#{user_id}', '#{ident_value}', "")")
   end
   #erzeugt einen neuen Datensatz für den User
   #generiert außerdem auch ein secret für die Datenbank
   def addUser(username)
-    secret = (0...32).map { (65 + rand(26)).chr }.join
+    secret = (0..32).map { (65 + rand(26)).chr }.join
     while !checkSecret(secret) do
-      secret = (0...32).map { (65 + rand(26)).chr }.join
+      secret = (0..32).map { (65 + rand(26)).chr }.join
     end
     @db.query("INSERT INTO `users` (`ID`, `username`, `secret`) VALUES (NULL, '#{username}', '#{secret}');")
     secret #return the secret
@@ -31,4 +38,4 @@ class ConfigManager
 end
 
 db_test = ConfigManager.new
-db_test.addUser('whitekiba')
+db_test.loadService('telegram')
