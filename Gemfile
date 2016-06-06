121549ca53774350267b85a8dc041a9e7fd92120
Sebastian Wessalowski <sebastian@wessalowski.org>
2016-06-06 19:46:00 +0200

    indent mit 2 spaces

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'Ruby-IRC'
-#gem "jabbot"
+gem "jabbot"
 gem 'mumble-ruby'
 gem 'sanitize'
 gem 'telegram-bot-ruby'
--- protocols/jabber_part.rb
+++ protocols/jabber_part.rb
@@ -1,54 +1,59 @@
 require 'rubygems'
 require 'jabbot'
-require
+require '../lib/module_base'
 
 class JabberBridge < ModuleBase
-	def self.start()
-		@my_name = "jabber"
+  def self.start()
+    @my_name = "jabber"
     @my_short = "J"
-		config = Jabbot::Config.new(
-			:login => conf[:id],
-			:password => conf[:pw],
-			:nick => conf[:name],
-			:server => conf[:conference_room].split('@')[1],
-			:channel => conf[:conference_room].split('@')[0],
-			:channel_password => conf[:channel_password],
-			:resource => conf[:resource]
-		)
-		@bot = Jabbot::Bot.new(config)
-		msg_handler = Jabbot::Handler.new do |msg, params|
-			handleMessage(msg)
-		end
-		join_handler = Jabbot::Handler.new do |msg, params|
-			handleJoin(msg)
-		end
-		leave_handler = Jabbot::Handler.new do |msg, params|
-			handleLeave(msg)
-		end
-		@bot.handlers[:message] << msg_handler
-		@bot.handlers[:join] << join_handler
-		@bot.handlers[:leave] << leave_handler
-		bridge.subscribe(@my_name)
-		bridge.addPrefix(@my_name, 'J')
-		Thread.new do
-			loop do
-				sleep 0.1
-				if msg_in = bridge.getNextMessage(@my_name)
-					@bot.send_message msg_in
-				end
-			end
-		end
-		@bot.connect
-	end
-	def self.handleMessage(message)
-		nick = message.user.split('@').first
-		@bridge.broadcast(@my_name, "[#{nick}]: #{message.text}")
-		$logger.info message.text
-	end
-	def self.handleJoin(message)
-		@bridge.broadcast(@my_name, " #{message.user} betrat den Chat.")
-	end
-	def self.handleLeave(message)
-		@bridge.broadcast(@my_name, " #{message.user} hat den Chat verlassen.")
-	end
+
+    @user_assoc = @db.loadService(@my_name)
+    @channels = @db.loadChatIDs(@my_name)
+    @channels_invert = @channels.invert
+
+    config = Jabbot::Config.new(
+      :login => conf[:id],
+      :password => conf[:pw],
+      :nick => conf[:name],
+      :server => conf[:conference_room].split('@')[1],
+      :channel => conf[:conference_room].split('@')[0],
+      :channel_password => conf[:channel_password],
+      :resource => conf[:resource]
+    )
+    @bot = Jabbot::Bot.new(config)
+    msg_handler = Jabbot::Handler.new do |msg, params|
+      handleMessage(msg)
+    end
+    join_handler = Jabbot::Handler.new do |msg, params|
+      handleJoin(msg)
+    end
+    leave_handler = Jabbot::Handler.new do |msg, params|
+      handleLeave(msg)
+    end
+    @bot.handlers[:message] << msg_handler
+    @bot.handlers[:join] << join_handler
+    @bot.handlers[:leave] << leave_handler
+    bridge.subscribe(@my_name)
+    bridge.addPrefix(@my_name, 'J')
+    Thread.new do
+      loop do
+        sleep 0.1
+        if msg_in = bridge.getNextMessage(@my_name)
+          @bot.send_message msg_in
+        end
+      end
+    end
+    @bot.connect
+  end
+  def self.handleMessage(message)
+    nick = message.user.split('@').first
+    @bridge.broadcast(@my_name, "[#{nick}]: #{message.text}")
+    $logger.info message.text
+  end
+  def self.handleJoin(message)
+    @bridge.broadcast(@my_name, " #{message.user} betrat den Chat.")
+  end
+  def self.handleLeave(message)
+    @bridge.broadcast(@my_name, " #{message.user} hat den Chat verlassen.")
+  end
 end
