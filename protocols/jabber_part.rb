8f207a945edf60351a01622a66382c24475ab6f5
Sebastian Wessalowski <sebastian@wessalowski.org>
2016-06-13 20:37:23 +0200

    Jabber läuft! \o/

--- protocols/jabber_part.rb
+++ protocols/jabber_part.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'xmpp4r'
 require 'xmpp4r/muc/helper/simplemucclient'
-require '../lib/module_base'
-require '../lib/db_manager'
+require_relative '../lib/module_base'
+require_relative '../lib/db_manager'
 
 $logger = Logger.new(File.dirname(__FILE__) + '/jabber.log')
 
@@ -19,7 +19,7 @@ class JabberBridge < ModuleBase
         begin
           Thread.new do
             startServer(server["ID"], server["server_url"], server["server_port"],
-                        server["user_name"], server["server_password"])
+                        server["user_name"], server["user_password"])
             $logger.info "Server #{server["server_url"]} started..."
           end
         rescue StandardError => e
@@ -35,6 +35,7 @@ class JabberBridge < ModuleBase
   def startServer(id, address, port, username, password)
     @db = DbManager.new
     $logger.info "New Jabber Server started."
+    $logger.debug "My credentials are: Username: #{username} and Password: #{password}"
     @muc = Hash.new
     @channels = @db.loadChannels(id)
     $logger.info @channels
@@ -51,32 +52,51 @@ class JabberBridge < ModuleBase
       $logger.info e
     end
     #wir schreiben unsere Channel in den @muc hash.
-    @channels_invert.each do |channel|
+    $logger.info @channels_invert
+    @channels_invert.each_value do |channel|
       $logger.info "Joining MUC #{channel}"
       begin
         @muc[channel] = Jabber::MUC::SimpleMUCClient.new(@bot)
-        @muc[channel].join("#{channel}@#{address}")
+        @muc[channel].join(channel)
         @muc[channel].on_message { |time,nick,text|
-          handleMessage(nick, channel, text)
+          unless time
+              if nick != "bridge"
+                handleMessage(nick, channel, text)
+              end
+          end
         }
       rescue StandardError => e
         $logger.info "Unable to join MUC. Stacktrace follows:"
         $logger.info e
       end
     end
+
+    #subscribe stuff
+    subscribe(@my_name)
     Thread.new do
       loop do
         sleep 0.1
-        if msg_in = bridge.getNextMessage(@my_name)
-          @bot.send_message(msg_in, @channels_invert[msg_in["user_id"]])
+        msg_in = @messages.pop
+        if !msg_in.nil?
+          $logger.info msg_in
+          begin
+            @muc[@channels_invert[msg_in["user_id"]]].say("[#{msg_in["source_network_type"]}][#{msg_in["nick"]}]#{msg_in["message"]}")
+          rescue StandardError => e
+            $logger.info "Unable to send message. Stacktrace follows:"
+            $logger.info e
+          end
         end
       end
     end
+    loop do
+      sleep 0.5
+    end
   end
   def handleMessage(nick, channel, message)
+    $logger.info "handleMessage called."
     $logger.info message
     self.publish(source_network_type: @my_short, source_network: @my_name,
-                 nick: nick, message: message.text, user_id: @channels[channel])
+                 nick: nick, message: message, user_id: @channels[channel])
   end
   def handleJoin(message)
     #@bridge.broadcast(@my_name, " #{message.user} betrat den Chat.")
@@ -87,4 +107,5 @@ class JabberBridge < ModuleBase
 end
 
 jb = JabberBridge.new
-jb.startServer(1, "rout0r.org", 5222, "bridge@rout0r.org", "Ulavewabe774")
+#jb.startServer(1, "rout0r.org", 5222, "bridge@rout0r.org", "Ulavewabe774")
+jb.startServers
