599b899fab86ea2c9643263ed820a00b96a731c0
Sebastian Wessalowski <sebastian@wessalowski.org>
2016-08-29 10:51:17 +0200

    Oops

--- lib/blacklist.rb
+++ lib/blacklist.rb
@@ -1,14 +1,14 @@
 require_relative '../lib/db_manager'
 
 class Blacklist
-  @blacklist = Hash.new
   def initialize
+    @blacklist = Hash.new
     db = DbManager.new
     blacklist_entrys = db.blacklist
     self.sort_entrys(blacklist_entrys)
   end
   def blacklisted?(user_id, username)
-    if @blacklist[user_id].include? username
+    if !@blacklist[user_id].nil? && @blacklist[user_id].include?(username)
       return true
     end
     false
