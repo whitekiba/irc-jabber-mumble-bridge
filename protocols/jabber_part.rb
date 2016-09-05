f61d6cb3be54b80b7cffdf1a5723ada3ebba991d
Sebastian Wessalowski <sebastian@wessalowski.org>
2016-09-05 10:35:35 +0200

    Jabber Fixes. Reload sollte nun existieren.
    Dass say nicht gefunden wird muss noch gefixt werden

--- protocols/jabber_part.rb
+++ protocols/jabber_part.rb
@@ -15,10 +15,6 @@ class JabberBridge < ModuleBase
     $logger.info 'New Jabber Server started.'
     $logger.debug "My credentials are: Username: #{username} and Password: #{password}"
     @muc = Hash.new
-    @channels = @db.loadChannels(id)
-    $logger.info @channels
-    #wird genutzt für die channel zu user zuordnung. channel name => user id
-    @channels_invert = @channels.invert
 
     begin
       @jid = Jabber::JID.new(username)
@@ -28,27 +24,6 @@ class JabberBridge < ModuleBase
       @bot.auth(password)
     rescue StandardError => e
       $logger.info e
-    end
-    #wir schreiben unsere Channel in den @muc hash.
-    $logger.info @channels_invert
-    @channels_invert.each_value do |channel|
-      $logger.info "Joining MUC #{channel}"
-      begin
-        @muc[channel] = Jabber::MUC::SimpleMUCClient.new(@bot)
-        @muc[channel].join(channel)
-        @muc[channel].on_message { |time,nick,text|
-          unless time
-              if nick != 'bridge'
-                handleMessage(nick, channel, text)
-              end
-          end
-        }
-        @muc[channel].on_join { |time, nick| handleJoin(nick) }
-        @muc[channel].on_leave { |time, nick| handleLeave(nick) }
-      rescue StandardError => e
-        $logger.error 'Unable to join MUC. Stacktrace follows:'
-        $logger.error e
-      end
     end
 
     #subscribe stuff
@@ -60,10 +35,11 @@ class JabberBridge < ModuleBase
         if !msg_in.nil?
           $logger.debug msg_in
           begin
-            @muc[@channels_invert[msg_in['user_id']]].say("[#{msg_in['source_network_type']}][#{msg_in['nick']}] #{msg_in['message']}")
+            @muc[@channels_invert[msg_in['user_id']]]
+                .say("[#{msg_in['source_network_type']}][#{msg_in['nick']}] #{msg_in['message']}")
           rescue StandardError => e
-            $logger.info 'Unable to send message. Stacktrace follows:'
-            $logger.info e
+            $logger.error 'Unable to send message. Stacktrace follows:'
+            $logger.error e
           end
         end
       end
@@ -84,8 +60,8 @@ class JabberBridge < ModuleBase
     end
   end
   def handleMessage(nick, channel, message)
-    $logger.info 'handleMessage called.'
-    $logger.info message
+    $logger.debug 'handleMessage called.'
+    $logger.debug message
     self.publish(source_network_type: @my_short, source_network: @my_name,
                  nick: nick, message: message, user_id: @channels[channel])
   end
@@ -99,11 +75,47 @@ class JabberBridge < ModuleBase
     self.publish(source_network_type: @my_short, source_network: @my_name,
                  nick: nick, user_id: @channels[channel], message_type: 'part')
   end
+
+  def join_channels
+    @channels = @db.loadChannels(@my_id)
+    $logger.debug @channels
+    #wird genutzt für die channel zu user zuordnung. channel name => user id
+    @channels_invert = @channels.invert
+
+    #wir schreiben unsere Channel in den @muc hash.
+    $logger.debug @channels_invert
+    @channels_invert.each_value do |channel|
+      join_channel(channel)
+    end
+  end
+
+  def join_channel(channel)
+    $logger.info "Joining MUC #{channel}"
+    begin
+      unless @muc[channel].nil?
+        @muc[channel] = Jabber::MUC::SimpleMUCClient.new(@bot)
+        @muc[channel].join(channel)
+        @muc[channel].on_message { |time,nick,text|
+          unless time
+            if nick != 'bridge'
+              handleMessage(nick, channel, text)
+            end
+          end
+        }
+        @muc[channel].on_join { |time, nick| handleJoin(nick) }
+        @muc[channel].on_leave { |time, nick| handleLeave(nick) }
+      end
+    rescue StandardError => e
+      $logger.error 'Unable to join MUC. Stacktrace follows:'
+      $logger.error e
+    end
+  end
+
   #Wir reloaden das Modul
   def reload
     begin
       $logger.info "Starting Jabber reload."
-      #TODO: Fehlt noch. Jabber hat aber erst mal keine Prio
+      join_channels #Wir joinen einfach alle Channel erneut. Die Methode handlet das
     rescue StandardError => e
       $logger.error "Reloading failed. Exception thrown:"
       $logger.error e
