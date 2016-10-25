293f7a456f1b45c8a02c006e2c6a8ca9a1457935
Sebastian Wessalowski <sebastian@wessalowski.org>
2016-10-25 10:12:23 +0200

    Tools ausgebaut

--- tools/reload.rb
+++ tools/reload.rb
@@ -1,11 +1,15 @@
 require 'redis'
 require 'json'
 
-redis_cmd = Redis.new(:host => '127.0.0.1', :port => 7777)
+if !ARGV[0].nil?
+  redis_cmd = Redis.new(:host => '127.0.0.1', :port => 7777)
 
-json = JSON.generate({
-    'cmd' => 'reload',
-    'args' => ''
-                     })
+  json = JSON.generate({
+      'cmd' => 'reload',
+      'args' => ''
+                       })
 
-redis_cmd.publish('cmd.irc', json)
+  redis_cmd.publish("cmd.#{ARGV[0]}", json)
+else
+  puts "reload.rb <server type>"
+end
